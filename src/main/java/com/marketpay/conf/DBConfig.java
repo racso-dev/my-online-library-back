840d156dda4d913a2e240fff3b57081560ec6218
Antony Tran <atran@steamulo.com>
2017-07-03 17:10:58 +0200

    [FIX] ajout des setters de DBConfig pour utiliser ConfigurationProperties

--- src/main/java/com/marketpay/conf/DBConfig.java
+++ src/main/java/com/marketpay/conf/DBConfig.java
@@ -1,11 +1,13 @@
 package com.marketpay.conf;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by etienne on 03/07/17.
  */
-@Configuration
+@Component
+@ConfigurationProperties(prefix = "database")
 public class DBConfig {
 
     private String driver;
@@ -43,4 +45,29 @@ public class DBConfig {
     public boolean isFlyway() {
         return flyway;
     }
+
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPackageName(String packageName) {
+        this.packageName = packageName;
+    }
+
+    public void setFlyway(boolean flyway) {
+        this.flyway = flyway;
+    }
+
 }
