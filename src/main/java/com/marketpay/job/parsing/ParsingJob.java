c9f8916ff12a86e27d80443ef7e3e5de072fbac4
tchekroun <tchekroun@Steamulos-MacBook-Pro.local>
2017-07-04 11:32:41 +0200

    [CLEAN] suppression du code mort

--- src/main/java/com/marketpay/job/parsing/ParsingJob.java
+++ src/main/java/com/marketpay/job/parsing/ParsingJob.java
@@ -7,11 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-
-/**
- * Created by etienne on 03/07/17.
- */
 @Component
 public class ParsingJob {
 
