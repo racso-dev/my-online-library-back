6d4bba0a90dacf8543f79fed6384d858c9713dd3
Antony Tran <atran@steamulo.com>
2017-07-05 15:06:34 +0200

    [EVOL] script table business unit

--- src/main/resources/db/init/V01__INIT_DB.sql
+++ src/main/resources/db/init/V01__INIT_DB.sql
@@ -1,26 +1,9 @@
-CREATE TABLE db_marketpay.transactions (
-  id BIGINT(20) NOT NULL AUTO_INCREMENT,
-  contract_number BIGINT(20) NOT NULL,
-  store_name VARCHAR(45) NOT NULL,
-  trade_date DATETIME(2) NOT NULL,
-  card_type VARCHAR(45) NOT NULL,
-  sens VARCHAR(45) NOT NULL,
-  gross_amount BIGINT(20) NOT NULL,
-  commission BIGINT(20) NOT NULL,
-  net_amount BIGINT(20) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE db_marketpay.users (
-  id BIGINT(20) NOT NULL AUTO_INCREMENT,
-  firstname VARCHAR(45) NOT NULL,
-  lastname VARCHAR(45) NOT NULL,
-  username VARCHAR(45) NOT NULL,
-  email VARCHAR(45) NOT NULL,
-  password VARCHAR(45) NOT NULL,
-  authorization TINYINT(1) NOT NULL,
-  admin TINYINT(1) NOT NULL,
-  datecreate DATETIME(2) NOT NULL,
-  datemodify DATETIME(2),
-  PRIMARY KEY (id)
-);
+CREATE TABLE `marketpay`.`business_unit` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `location` VARCHAR(2) NOT NULL,
+  `bank_account_name` VARCHAR(45) NOT NULL,
+  `bank_account_number` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `bank_account_number_UNIQUE` (`bank_account_number` ASC)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
