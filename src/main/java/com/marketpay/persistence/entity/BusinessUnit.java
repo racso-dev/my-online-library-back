038e6acb4191657a0297815db41607f22d24f09d
Antony Tran <atran@steamulo.com>
2017-07-06 14:34:11 +0200

    [EVOL] repository et entity bu

---
+++ src/main/java/com/marketpay/persistence/entity/BusinessUnit.java
@@ -0,0 +1,28 @@
+package com.marketpay.persistence.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by antony on 06/07/17.
+ */
+@Entity
+public class BusinessUnit {
+    private String name;
+    private String location;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
