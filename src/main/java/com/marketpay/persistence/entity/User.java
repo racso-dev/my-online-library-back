bc66c63457652c184d69b73247a89a3593dec4c3
Antony Tran <atran@steamulo.com>
2017-07-06 14:50:49 +0200

    [EVOL] repository et entity user

---
+++ src/main/java/com/marketpay/persistence/entity/User.java
@@ -0,0 +1,64 @@
+package com.marketpay.persistence.entity;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by antony on 06/07/17.
+ */
+@Entity
+public class User {
+    private long profile;
+    private String email;
+    private String login;
+    private String password;
+    private long idBu;
+    private String idStore;
+
+    public long getProfile() {
+        return profile;
+    }
+
+    public void setProfile(long profile) {
+        this.profile = profile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public long getIdBu() {
+        return idBu;
+    }
+
+    public void setIdBu(long idBu) {
+        this.idBu = idBu;
+    }
+
+    public String getIdStore() {
+        return idStore;
+    }
+
+    public void setIdStore(String idStore) {
+        this.idStore = idStore;
+    }
+}
