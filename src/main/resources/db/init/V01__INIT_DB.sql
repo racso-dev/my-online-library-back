1acf9dc7b8fadb13e57b58aa08ecbed5efabf0c3
Antony Tran <atran@steamulo.com>
2017-07-06 11:38:40 +0200

    [FIX MR] auto increment id

--- src/main/resources/db/init/V01__INIT_DB.sql
+++ src/main/resources/db/init/V01__INIT_DB.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `marketpay`.`business_unit` (
-  `id` BIGINT(20) NOT NULL,
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   `location` VARCHAR(2) NOT NULL,
   PRIMARY KEY (`id`))
