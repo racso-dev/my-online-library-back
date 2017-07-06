71a14796de6396037179567992cbce86b6bc5260
Antony Tran <atran@steamulo.com>
2017-07-06 10:24:36 +0200

    [EVOL] script table user et transaction

--- src/main/resources/db/init/V01__INIT_DB.sql
+++ src/main/resources/db/init/V01__INIT_DB.sql
@@ -22,3 +22,41 @@ CREATE TABLE `marketpay`.`store` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
     ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `marketpay`.`users` (
+  `id` INT NOT NULL,
+  `profile` SMALLINT(5) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `account_number_bu` VARCHAR(45) NULL,
+  `id_store` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC),
+  INDEX `account_number_bu_idx` (`account_number_bu` ASC),
+  INDEX `id_store_idx` (`id_store` ASC),
+  CONSTRAINT `account_number_bu`
+    FOREIGN KEY (`account_number_bu`)
+    REFERENCES `marketpay`.`business_unit` (`bank_account_number`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_store`
+    FOREIGN KEY (`id_store`)
+    REFERENCES `marketpay`.`store` (`contract_number`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `marketpay`.`transactions` (
+  `id` INT NOT NULL,
+  `funding_date` DATETIME NOT NULL,
+  `id_client` BIGINT(20) NOT NULL,
+  `trade_date` DATETIME NOT NULL,
+  `card_type` VARCHAR(45) NOT NULL,
+  `sens` VARCHAR(45) NOT NULL,
+  `gross_amount` BIGINT(20) NOT NULL,
+  `net_amount` BIGINT(20) NOT NULL,
+  `contract_number` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `funding_date_idx` (`funding_date` ASC),
+  INDEX `contract_number_idx` (`contract_number` ASC))
+  ENGINE=InnoDB DEFAULT CHARSET=utf8;
