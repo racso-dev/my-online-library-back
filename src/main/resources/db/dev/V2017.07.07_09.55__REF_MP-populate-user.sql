2ca17663c6a4b0eddf09edffa236bdd7152ee9a4
Antony Tran <atran@steamulo.com>
2017-07-07 10:15:39 +0200

    [EVOL] script populate user

---
+++ src/main/resources/db/dev/V2017.07.07_09.55__REF_MP-populate-user.sql
@@ -0,0 +1,16 @@
+INSERT INTO user (profile, email, login, password, id_bu, id_store)
+  values (1, 'admin@steamulo.com', 'admin', 'admin', null, null);
+
+INSERT INTO user (profile, email, login, password, id_bu, id_store)
+  values (1, 'su@steamulo.com', 'super', 'super', 1, null);
+INSERT INTO user (profile, email, login, password, id_bu, id_store)
+  values (1, 'su2@steamulo.com', 'super2', 'super2', 2, null);
+INSERT INTO user (profile, email, login, password, id_bu, id_store)
+  values (1, 'su3@steamulo.com', 'super3', 'super3', 3, null);
+
+INSERT INTO user (profile, email, login, password, id_bu, id_store)
+  values (1, 'user@steamulo.com', 'user', 'user', 1, '123');
+INSERT INTO user (profile, email, login, password, id_bu, id_store)
+  values (1, 'user2@steamulo.com', 'user2', 'user2', 2, '1234');
+INSERT INTO user (profile, email, login, password, id_bu, id_store)
+  values (1, 'user3@steamulo.com', 'user3', 'user3', 3, '1235');
