8be60a9be7c0fbf7305e4cd944cae5e4336f302b
Antony Tran <atran@steamulo.com>
2017-07-07 09:50:52 +0200

    [EVOL] script populate business unit

---
+++ src/main/resources/db/dev/V2017.07.07_09.47__REF_MP-populate-bu.sql
@@ -0,0 +1,4 @@
+INSERT INTO business_unit (name, location) values ('Carrefour Anto', 'FR');
+INSERT INTO business_unit (name, location) values ('Carrefour Thomatoto', 'FR');
+INSERT INTO business_unit (name, location) values ('Carrefour Etienne', 'FR');
+INSERT INTO business_unit (name, location) values ('Carrefour SGouGou', 'FR');
