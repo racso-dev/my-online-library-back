9f5fa51dc17d7a0b6361ac232dbebb03837078ce
Sylvain Gourio <sgourio@steamulo.com>
2017-07-10 18:46:58 +0200

    Fix allow actuator URLs

--- src/main/java/com/marketpay/conf/security/WebSecurityConfig.java
+++ src/main/java/com/marketpay/conf/security/WebSecurityConfig.java
@@ -23,6 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
             .antMatchers("/").permitAll()
+            .antMatchers("/manage/**").permitAll()
             .antMatchers(HttpMethod.POST, "/login").permitAll()
             .antMatchers("/api/**").authenticated()
                 .and()
