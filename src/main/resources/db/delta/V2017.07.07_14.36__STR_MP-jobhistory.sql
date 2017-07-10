44174d26d8d38b35d32014e877ffc57c53507665
Etienne Gay <egay@steamulo.com>
2017-07-10 19:29:04 +0200

    [FIX] correction deltas BDD

--- src/main/resources/db/delta/V2017.07.07_14.36__STR_MP-jobhistory.sql
+++ src/main/resources/db/delta/V2017.07.07_14.36__STR_MP-jobhistory.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `marketpay`.`job_history` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `date` DATETIME(2) NOT NULL,
+  `date` DATETIME NOT NULL,
   `status` INT NOT NULL,
   `filename` VARCHAR(45) NOT NULL,
   `filetype` VARCHAR(45) NOT NULL,
