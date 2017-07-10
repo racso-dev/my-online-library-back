4ec3a25ba1807d94a327c62a925d0c28eec4cc76
tchekroun <tchekroun@Steamulos-MacBook-Pro.local>
2017-07-10 17:25:24 +0200

    [CLEAN] Clean code

--- src/main/java/com/marketpay/persistence/converter/LocalDateTimeAttributeConverter.java
+++ src/main/java/com/marketpay/persistence/converter/LocalDateTimeAttributeConverter.java
@@ -10,12 +10,12 @@ import java.time.LocalDateTime;
 public class LocalDateTimeAttributeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
 
     @Override
-    public java.sql.Timestamp convertToDatabaseColumn(java.time.LocalDateTime entityValue) {
-        return entityValue == null ? null : java.sql.Timestamp.valueOf(entityValue);
+    public Timestamp convertToDatabaseColumn(LocalDateTime entityValue) {
+        return entityValue == null ? null : Timestamp.valueOf(entityValue);
     }
 
     @Override
-    public java.time.LocalDateTime convertToEntityAttribute(java.sql.Timestamp dbValue) {
+    public LocalDateTime convertToEntityAttribute(Timestamp dbValue) {
         return dbValue == null ? null : dbValue.toLocalDateTime();
     }
 }
