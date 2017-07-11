81b517e2a272571beb303a783297af58db75b432
tchekroun <tchekroun@Steamulos-MacBook-Pro.local>
2017-07-11 10:12:08 +0200

    [ADD] Récupération des transactions a une date donnée

--- src/main/java/com/marketpay/api/operation/OperationController.java
+++ src/main/java/com/marketpay/api/operation/OperationController.java
@@ -4,6 +4,7 @@ import com.marketpay.api.MarketPayController;
 import com.marketpay.api.operation.response.OperationListResponse;
 import com.marketpay.services.operation.OperationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -27,11 +28,13 @@ public class OperationController extends MarketPayController {
      */
     @RequestMapping(value = "", method = RequestMethod.GET)
     public @ResponseBody
-    OperationListResponse getOperationListByDate(@RequestParam(value = "localDate") LocalDate localDate) {
-        // TODO: Récupérrer la liste de store associé à l'utilisateur connecté via la session
+    OperationListResponse getOperationListByDate(@RequestParam(value = "localDate") @DateTimeFormat(pattern = "yyyyMMdd") LocalDate localDate) {
         OperationListResponse response = new OperationListResponse();
 
+        // TODO: Récupérrer la liste de store associé à l'utilisateur connecté via la session
         List<Long> storeIdList = new ArrayList<>();
+        storeIdList.add(2l);
+        storeIdList.add(1l);
         response.setOperationList(operationService.getOperationFromStoreIdListAndLocalDate(localDate, storeIdList));
 
         return response;
