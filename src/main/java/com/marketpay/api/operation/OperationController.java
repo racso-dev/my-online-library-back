65c5138daf1f0924f90f4f2858d94f90c775ecc0
tchekroun <tchekroun@Steamulos-MacBook-Pro.local>
2017-07-11 09:14:54 +0200

    [ADD] Création du WS operation et du model de la reponse

--- src/main/java/com/marketpay/api/operation/OperationController.java
+++ src/main/java/com/marketpay/api/operation/OperationController.java
@@ -1,7 +1,30 @@
 package com.marketpay.api.operation;
 
+import com.marketpay.api.MarketPayController;
+import com.marketpay.api.operation.response.OperationListResponse;
+import com.marketpay.services.operation.OperationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
 /**
  * Created by tchekroun on 10/07/2017.
  */
-public class OperationController {
+@RestController
+@RequestMapping(value = "/operation")
+public class OperationController extends MarketPayController {
+
+    @Autowired
+    private OperationService operationService;
+
+    /**
+     * Hello world d'exemple
+     * @return
+     */
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public @ResponseBody
+    OperationListResponse getOperationListByDate(@RequestParam(value = "localDate") LocalDate localDate) {
+        //return operationService.getOperationFromStoreIdListAndLocalDate(localDate, )
+    }
 }
--- src/main/java/com/marketpay/api/operation/response/OperationListResponse.java
+++ src/main/java/com/marketpay/api/operation/response/OperationListResponse.java
@@ -1,7 +1,13 @@
 package com.marketpay.api.operation.response;
 
+import com.marketpay.persistence.entity.Operation;
+
+import java.util.List;
+
 /**
  * Created by tchekroun on 10/07/2017.
  */
 public class OperationListResponse {
+    private List<Operation> operationList;
+
 }
