8084d2dca23ec8e91d878e83428b5c2a54a09aa7
Etienne Gay <egay@steamulo.com>
2017-08-01 15:46:55 +0200

    [CLEAN] delete CORS filter

--- src/main/java/com/marketpay/conf/security/WebSecurityConfig.java
+++ src/main/java/com/marketpay/conf/security/WebSecurityConfig.java
@@ -1,24 +1,19 @@
 package com.marketpay.conf.security;
 
-import com.marketpay.filter.CORSFilter;
 import com.marketpay.filter.security.JWTAuthenticationFilter;
 import com.marketpay.filter.security.JWTLoginFilter;
 import com.marketpay.services.auth.MarketPayUserDetailsService;
 import com.marketpay.services.auth.TokenAuthenticationService;
 import com.marketpay.utils.PasswordUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.sql.DataSource;
 
 /**
  * Created by sgourio on 10/07/2017.
@@ -48,8 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
             // We filter the api/login requests
             .addFilterBefore(new JWTLoginFilter("/login", authenticationManager(), tokenAuthenticationService), UsernamePasswordAuthenticationFilter.class)
             // And filter other requests to check the presence of JWT in header
-            .addFilterBefore(new JWTAuthenticationFilter(tokenAuthenticationService), UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(new CORSFilter(), JWTLoginFilter.class);
+            .addFilterBefore(new JWTAuthenticationFilter(tokenAuthenticationService), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Autowired
