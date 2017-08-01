08fd60bbc72415a4e00226e2e7ba5a6ff8c0c622
Etienne Gay <egay@steamulo.com>
2017-08-01 18:41:58 +0200

    [CLEAN] change service login, plus de cookie token dans la response

--- src/main/java/com/marketpay/services/auth/TokenAuthenticationService.java
+++ src/main/java/com/marketpay/services/auth/TokenAuthenticationService.java
@@ -1,7 +1,10 @@
 package com.marketpay.services.auth;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.marketpay.exception.MarketPayException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.security
     .authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Date;
 
 import static java.util.Collections.emptyList;
@@ -24,15 +28,17 @@ public class TokenAuthenticationService {
     private String SECRET;
     private final String COOKIE_NAME = "sid";
 
-    public void addAuthentication(HttpServletRequest req, HttpServletResponse res, String username) {
+    public void addAuthentication(HttpServletRequest req, HttpServletResponse res, String username) throws IOException {
         String JWT = Jwts.builder()
             .setPayload("{\"sub\":\"" + username +"\", \"profile\":\"test\", \"exp\":"+new Date(System.currentTimeMillis() + EXPIRATIONTIME).getTime()+"}")
             //.setSubject(username)
             //.setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
             .signWith(SignatureAlgorithm.HS512, SECRET)
             .compact();
-        Cookie sessionCookie = new Cookie(COOKIE_NAME, JWT );
-        res.addCookie(sessionCookie);
+            res.setContentType("application/json");
+
+        ObjectMapper mapper = new ObjectMapper();
+        res.getWriter().write(mapper.writeValueAsString(new TokenResponse(JWT)));
     }
 
     public Authentication getAuthentication(HttpServletRequest request) {
