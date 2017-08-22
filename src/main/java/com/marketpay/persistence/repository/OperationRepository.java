fbe1054581f9002ddd7842d5bdac5f1b9ca78500
tchekroun <tchekroun@Steamulos-MacBook-Pro.local>
2017-08-22 10:52:39 +0200

    [FIX] Variable name

--- src/main/java/com/marketpay/persistence/repository/OperationRepository.java
+++ src/main/java/com/marketpay/persistence/repository/OperationRepository.java
@@ -16,5 +16,5 @@ public interface OperationRepository extends OptionalCRUDRepository<Operation, L
     List<Operation> findByContractNumberAndIdShopIsNull(String contractNumber);
     Optional<Operation> findFirstByIdShopInOrderByFundingDateDesc(List<Long> idShopList);
     List<Operation> findByCreateDateAndIdShopInAndFundingDate(LocalDate createDate, List<Long> idShopList, LocalDate date);
-    List<Operation> findByIdJobHistory(long jobHistory);
+    List<Operation> findByIdJobHistory(long idJobHistory);
 }
