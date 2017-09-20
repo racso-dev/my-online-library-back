d2b229374af89793e8c5ad88962fd6254f6dac9c
Etienne Gay <egay@steamulo.com>
2017-09-20 11:49:37 +0200

    Clean conf, resources

--- src/test/java/com/steamulo/MarketPayUnitTests.java
+++ src/test/java/com/steamulo/MarketPayUnitTests.java
@@ -1,16 +1,6 @@
 package com.steamulo;
 
-import com.steamulo.persistence.entity.*;
-import com.steamulo.persistence.repository.*;
-import org.junit.Before;
-import org.mockito.Matchers;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Optional;
 
 /**
  * Created by tchekroun on 10/07/2017.
