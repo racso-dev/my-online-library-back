3180f1b8337f66cbf59d15e4ec34b09f9b20145d
dle <dle@steamulo.com>
2017-09-20 15:42:44 +0200

    [ADD] classes custom de serialisation/déserialisation pour les dates

---
+++ src/main/java/com/steamulo/utils/deserializer/LocalDateTimeDeserializer.java
@@ -0,0 +1,26 @@
+package com.steamulo.utils.deserializer;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.node.TextNode;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by dle on 20/09/17
+ */
+public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    @Override
+    public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        ObjectCodec oc = jsonParser.getCodec();
+        TextNode node = oc.readTree(jsonParser);
+        String dateString = node.textValue();
+        return LocalDateTime.from(formatter.parse(dateString));
+    }
+}
---
+++ src/main/java/com/steamulo/utils/serializer/LocalDateTimeSerializer.java
@@ -0,0 +1,21 @@
+package com.steamulo.utils.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Created by dle on 20/09/17
+ */
+public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+
+    @Override
+    public void serialize(LocalDateTime localDateTime, JsonGenerator jsonGenerator, SerializerProvider serializers) throws IOException {
+        jsonGenerator.writeString(localDateTime.format(formatter));
+    }
+}
